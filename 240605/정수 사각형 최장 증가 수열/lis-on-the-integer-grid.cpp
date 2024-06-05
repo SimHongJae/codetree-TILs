4837f50b2a9192c9a6a341e7ca60f346150e91fa
DB <103403855+SimHongJae@users.noreply.github.com>
2024-06-05 22:43:26 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 69ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240605/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct Point{
+    int x;
+    int y;
+}PT;
+
+int arr[1000][1000];
+int ans[1000][1000];
+
+
+int main() {
+    int n;
+   
+    int max_num = 0;
+    PT max_pt;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+            max_num = max(max_num,arr[i][j]);
+            if(max_num == arr[i][j]){
+                max_pt.x = i;
+                max_pt.y = j;
+            }
+        }
+    }
+    ans[max_pt.x][max_pt.y] = 1;
+    int dx[4]={1,-1,0,0};
+    int dy[4]={0,0,1,-1};
+
+    queue<PT> q;
+    q.push(max_pt);
+    while(!q.empty()){
+        PT a = q.front();
+        q.pop();
+        for(int i=0;i<4;i++){
+            PT next;
+            next.x = a.x+dx[i];
+            next.y = a.y+dy[i];
+            if(next.x<0)continue;
+            if(next.y<0)continue;
+            if(next.x>=n)continue;
+            if(next.y>=n)continue;
+
+            if(arr[a.x][a.y]>arr[next.x][next.y]){
+                if(ans[next.x][next.y]<ans[a.x][a.y]+1){
+                    q.push(next);
+                    ans[next.x][next.y] = ans[a.x][a.y]+1;
+                }
+            }
+        }
+    }
+
+    int max_ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            
+            max_ans = max(max_ans,ans[i][j]);
+            
+        }
+    }
+
+    cout<<max_ans;
+    return 0;
+}
